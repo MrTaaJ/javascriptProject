74b8d1fb80ed546612c0d142697f41b9df910318
Saheed Tajudeen <saheed.taaj@gmail.com>
2022-03-07 18:03:58 +0100

    fix: fix the loading data from localStorage bug upon reload of first load

--- js/index.js
+++ js/index.js
@@ -5,74 +5,65 @@ const bookContainer = document.querySelector('.book-container');
 
 let booksInfo = [];
 
-const getID = () =>{
-  return '_'+Math.random().toString(36).substring(2,9);
-}
+const getID = () => `_${Math.random().toString(36).substring(2, 9)}`;
 
 const setLocalStorage = (booksInfo) => {
   localStorage.setItem('storeLocal', JSON.stringify(booksInfo));
 };
 
-const removeLocalStorage = () => {
-  localStorage.removeItem('storeLocal');
+const clearData = (title, author) => {
+  title.value = '';
+  author.value = '';
 };
 
-const clearData = (title, author) => {
-  title.value = "";
-  author.value = "";
-}
-
-const addData = (title, author) =>{
-  const bookInfo = {id: getID(), title: "", author: ""};
+const addData = (title, author) => {
+  const bookInfo = { id: getID(), title: '', author: '' };
   bookInfo.title = title.value;
   bookInfo.author = author.value;
   booksInfo.push(bookInfo);
-}
+};
 
 const updateBook = (newBook) => {
   let displayData = '';
   newBook.forEach((element) => {
-      displayData += `<div class="book">
+    displayData += `<div class="book">
       <p>${element.title}</p>
       <p>${element.author}</p>
       <button id="${element.id}" class="remove-button">Remove</button>
       </div>`;
   });
   bookContainer.innerHTML = displayData;
-}
+};
 
 const addBook = (e) => {
   e.preventDefault();
   addData(title, author);
+  setLocalStorage(booksInfo);
   updateBook(booksInfo);
   clearData(title, author);
-  setLocalStorage(booksInfo);
-}
+};
 
 const removeData = (e) => {
   e.preventDefault();
   let id;
-  if(e.target.classList.contains("remove-button")){
+  if (e.target.classList.contains('remove-button')) {
     id = e.target.id;
   }
-  booksInfo = booksInfo.filter((book)=>{
-    return book.id !== id;
-  })
+  booksInfo = booksInfo.filter((book) => book.id !== id);
   setLocalStorage(booksInfo);
   updateBook(booksInfo);
-  
-}
+};
 
 const getLocal = () => {
   const returnItem = localStorage.getItem('storeLocal');
   if (returnItem) {
-    const returnItemObject = JSON.parse(returnItem);
-    updateBook(returnItemObject);
+    booksInfo = JSON.parse(returnItem);
+    updateBook(booksInfo);
   }
 };
 
 window.addEventListener('load', getLocal);
 
-newBooks.addEventListener("submit", addBook);
+newBooks.addEventListener('submit', addBook);
 
-bookContainer.addEventListener("click", removeData);
+bookContainer.addEventListener('click', removeData);
