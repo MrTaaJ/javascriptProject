690efd4945287d485012a1827edc36da768c8f8e
Saheed Tajudeen <saheed.taaj@gmail.com>
2022-03-07 17:16:23 +0100

    feat: update add and delete feature and working perfectly

--- js/index.js
+++ js/index.js
@@ -2,28 +2,33 @@ const title = document.querySelector('.title');
 const author = document.querySelector('.author');
 const newBooks = document.querySelector('.form');
 const bookContainer = document.querySelector('.book-container');
-const removeBook = document.querySelectorAll('.remove-button')
+// const bookContainer = document.querySelectorAll('.book-container');
 
 let booksInfo = [];
 
+const getID = () =>{
+  return '_'+Math.random().toString(36).substring(2,9);
+}
+
+const clearData = (title, author) => {
+  title.value = "";
+  author.value = "";
+}
+
 const addData = (title, author) =>{
-  const bookInfo = {title: "", author: ""};
+  const bookInfo = {id: getID(), title: "", author: ""};
   bookInfo.title = title.value;
   bookInfo.author = author.value;
   booksInfo.push(bookInfo);
 }
 
-const removeData = () => {
-
-}
-
 const updateBook = (newBook) => {
   let displayData = '';
-  newBook.forEach(element => {
+  newBook.forEach((element, index) => {
       displayData += `<div class="book">
       <p>${element.title}</p>
       <p>${element.author}</p>
-      <button class="remove-button">Remove</button>
+      <button id="${element.id}" class="remove-button">Remove</button>
       </div>`;
   });
   bookContainer.innerHTML = displayData;
@@ -33,18 +38,22 @@ const addBook = (e) => {
   e.preventDefault();
   addData(title, author);
   updateBook(booksInfo);
+  clearData(title, author);
   console.log(booksInfo);
 }
 
-newBooks.addEventListener("submit", addBook);
-
-let indexofDelete;
-
-for (let i = 0; i < booksInfo.length; i += 1) {
-  removeBook[i].addEventListener('click', () => {
-    booksInfo = booksInfo.filter((book)=>{
-      return book.author !== booksInfo[i].author;
-    })
-  });
+const removeData = (e) => {
+  e.preventDefault();
+  let id;
+  if(e.target.classList.contains("remove-button")){
+    id = e.target.id;
+  }
+  booksInfo = booksInfo.filter((book)=>{
+    return book.id !== id;
+  })
+  updateBook(booksInfo);
+  
 }
 
+newBooks.addEventListener("submit", addBook);
+bookContainer.addEventListener("click", removeData);
