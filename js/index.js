d349b77d6b8da4f92d7c1744940a06fadf47f8f4
Saheed Tajudeen <saheed.taaj@gmail.com>
2022-03-20 13:05:33 +0100

    feat: remove numbering and made the code dry

--- js/index.js
+++ js/index.js
@@ -16,33 +16,12 @@ const getID = () => {
   return id;
 };
 
-const updateBook = newBook => {
-  newBook.forEach((element, index) => {
-    const tableRow = document.createElement('tr');
-    const tableData1 = document.createElement('td');
-    const tableData2 = document.createElement('td');
-    const tableData3 = document.createElement('td');
-    const tableData4 = document.createElement('td');
-    const button = document.createElement('button');
-    tableRow.className = 'book';
-    button.id = element.id;
-    button.className = 'remove-button';
-    button.innerText = 'Remove';
-    tableData1.innerText = index + 1;
-    tableData2.innerText = element.author;
-    tableData3.innerText = element.title;
-    tableData4.append(button);
-    tableRow.append(tableData1, tableData2, tableData3, tableData4);
-    bookContainer.append(tableRow);
-  });
-};
-
 class BooksInfoData {
-  constructor(title, author, id) {
+  constructor(title, author, id, container) {
     this.title = title;
     this.author = author;
     this.id = id;
-    this.bookContainer = document.querySelector('.book-container');
+    this.container = container;
   }
 
   clearData() {
@@ -57,51 +36,39 @@ class BooksInfoData {
     return bookInfo;
   }
 
-  updateNumber() {
-    const updateList = Array.from(this.bookContainer.children);
-    let index = 0;
-    updateList.forEach(element => {
-      element.firstChild.innerText = index + 1;
-      index += 1;
-    });
-  }
-
-  updateNewBook() {
+  updateNewBook(newBook) {
+    const { id, title, author } = newBook;
     const tableRow = document.createElement('tr');
     const tableData1 = document.createElement('td');
     const tableData2 = document.createElement('td');
     const tableData3 = document.createElement('td');
-    const tableData4 = document.createElement('td');
     const button = document.createElement('button');
     tableRow.className = 'book';
-    button.id = this.id;
+    button.id = id;
     button.className = 'remove-button';
     button.innerText = 'Remove';
-    tableData1.innerText = 1;
-    tableData2.innerText = this.author.value;
-    tableData3.innerText = this.title.value;
-    tableData4.append(button);
-    tableRow.append(tableData1, tableData2, tableData3, tableData4);
-    this.bookContainer.append(tableRow);
-    this.updateNumber();
+    tableData1.innerText = author;
+    tableData2.innerText = title;
+    tableData3.append(button);
+    tableRow.append(tableData1, tableData2, tableData3);
+    this.container.append(tableRow);
   }
 
   deleteBook() {
     booksInfo = booksInfo.filter(book => book.id !== this.id);
     const bookId = document.querySelector(`#${this.id}`);
     const bookParent = bookId.parentElement.parentElement;
-    this.bookContainer.removeChild(bookParent);
-    this.updateNumber();
+    this.container.removeChild(bookParent);
     setLocalStorage(booksInfo);
   }
 }
 
 const addBook = e => {
   e.preventDefault();
-  const newClassBook = new BooksInfoData(title, author, getID());
+  const newClassBook = new BooksInfoData(title, author, getID(), bookContainer);
   booksInfo.push(newClassBook.addData());
   setLocalStorage(booksInfo);
-  newClassBook.updateNewBook();
+  newClassBook.updateNewBook(newClassBook.addData());
   newClassBook.clearData();
 };
 
@@ -111,8 +78,15 @@ const removeData = e => {
   if (e.target.classList.contains('remove-button')) {
     id = e.target.id;
   }
-  const newClassBook = new BooksInfoData(title, author, id);
+  const newClassBook = new BooksInfoData(title, author, id, bookContainer);
   newClassBook.deleteBook();
+};
+
+const updateBook = newBook => {
+  const newClassBook = new BooksInfoData(title, author, getID(), bookContainer);
+  newBook.forEach(element => {
+    newClassBook.updateNewBook(element);
+  });
 };
 
 const getLocal = () => {
